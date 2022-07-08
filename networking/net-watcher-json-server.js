d7068c43fd7e8ca9d7ba7bdb0dcf19cff9cf6562
Donald Dominic <gotodonald@gmail.com>
2022-07-08 10:39:32 +0100

    Creating server connection with net module

---
+++ networking/net-watcher-json-server.js
@@ -0,0 +1,25 @@
+"use strict";
+const net = require("net");
+const fs = require("fs");
+
+try {
+  const filename = process.argv[2];
+  net
+    .createServer((connection) => {
+      console.log("Subscriber connected");
+      console.log(`Now watching ${filename} for changes...\n`);
+
+      const watcher = fs.watch(filename, () =>
+        connection.write(`File changed: ${new Date()}\n`)
+      );
+
+      // cleanup
+      connection.on("close", () => {
+        console.log("Subsciber disconnected");
+        watcher.close();
+      });
+    })
+    .listen(60300, () => console.log("Listening for subscribers..."));
+} catch (error) {
+  console.log(error.message);
+}
