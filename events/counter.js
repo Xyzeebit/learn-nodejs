64170c304d80e5d068757ed1927c2cb82f8fbb11
Donald Dominic <gotodonald@gmail.com>
2022-07-15 09:23:08 +0100

    counter event

--- events/counter.js
+++ events/counter.js
@@ -1,25 +1,31 @@
-'use-strict';
-const EventEmitter = require('events').EventEmitter;
+"use-strict";
+const EventEmitter = require("events").EventEmitter;
 
 class Counter extends EventEmitter {
-    
-    constructor(i) {
-        super();
-        this.i = i;
-    }
-    increment () {
-        this.i++;
-        this.emit('incremented', this.i) // Emits and event name increment
-    }
+  constructor(i) {
+    super();
+    this.i = i;
+  }
+  increment() {
+    this.i++;
+    this.emit("incremented", this.i); // Emits and event name incremented
+  }
+  decrement() {
+    this.i--;
+    this.emit("decremented", this.i); // Emits and event name decremented
+  }
 }
-
 
 const counter = new Counter(10);
 
 const callback = function (n) {
-    console.log(n);
-}
+  console.log(n);
+};
 
-counter.on('incremented', callback);
+counter.on("incremented", callback);
+counter.on('decremented', n => {
+    console.log(n);
+})
 counter.increment();
 counter.increment();
+counter.decrement();
